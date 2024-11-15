a3ad29d715fb6a8ec37449023d3cd4000974fc22
MangeshChaudhari2000 <157056611+MangeshChaudhari2000@users.noreply.github.com>
2024-11-16 03:30:46 +0530

    updated hover effect

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,16 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import { PROJECTS } from "../constants";
 import { MdArrowOutward } from "react-icons/md";
 import { motion } from "framer-motion";
 
 const Projects = () => {
-  const [activeProject, setActiveProject] = useState(null);
-
-  const handleClick = (index) => {
-    // Toggle the active project on click
-    setActiveProject(activeProject === index ? null : index);
-  };
-
   return (
     <section className="pt-20" id="projects">
       <motion.h2
@@ -25,13 +18,11 @@ const Projects = () => {
         {PROJECTS.map((project, index) => (
           <motion.div
             initial={{ opacity: 0, scale: 0.9 }}
-            animate={{ opacity: 1, scale: 1 }}
+            whileInView={{ opacity: 1, scale: 1 }}
             transition={{ duration: 0.5 }}
+            whileHover={{scale:1}}
             key={project.id}
-            className="group relative overflow-hidden rounded-3xl cursor-pointer"
-            onClick={() => handleClick(index)} // Handle click on mobile
-            onMouseEnter={() => setActiveProject(index)} // Show on hover for desktop
-            onMouseLeave={() => setActiveProject(null)} // Hide on mouse leave for desktop
+            className="group relative overflow-hidden rounded-3xl"
           >
             <motion.img
               whileHover={{ scale: 1.1 }}
@@ -40,18 +31,16 @@ const Projects = () => {
               className="w-full h-full object-cover duration-500 group-hover:scale-110"
             />
             <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: activeProject === index ? 1 : 0 }} // Show overlay based on active project
-              transition={{ duration: 0.5 }}
-              className="absolute inset-0 flex flex-col items-center justify-center text-white backdrop-blur-lg transition-opacity duration-500"
+              
+              className="absolute inset-0 flex flex-col items-center justify-center text-white opacity-0 backdrop-blur-lg transition-opacity duration-500 hover:opacity-100"
             >
               <h3 className="mb-2 text-xl">{project.name}</h3>
               <p className="mb-12 p-4">{project.description}</p>
               <a
                 href={project.githubLink}
                 target="_blank"
-                rel="noopener noreferrer"
-                className="rounded-full bg-white px-4 py-2 text-black hover:bg-gray-300"
+                rel="noopener noreferer"
+                className="rounded-full bg-white  px-4 py-2 text-black hover:bg-gray-300"
               >
                 <div className="flex items-center">
                   <span>View on GitHub</span>
